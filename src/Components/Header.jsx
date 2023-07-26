f5c82991bd1730338ef513a812220a64a3334ed0
Lakshmi Divya Kolora <divyakolora@gmail.com>
2023-07-26 08:59:36 -0600

    commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,3 @@
 export default function Header() {
-    return(
-        <div>The Last NetBender</div>
-    )
+  return <div className="Header">The Last NetBender</div>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "./Store/store.jsx";
-import Characters from "./Components/Characters.jsx"
+import Characters from "./Components/Characters.jsx";
 
 const store = configureStore();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-    <Provider store={store}>
-      <App />
+      <Provider store={store}>
+        <App />
       </Provider>
     </Router>
   </React.StrictMode>
