4d4499adac0f34b1f855355c74a1cca295395b2a
Lakshmi Divya Kolora <divyakolora@gmail.com>
2023-07-25 20:22:46 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# LastNetBender_React
-
 Deploy link: https://ornate-tulumba-99839d.netlify.app/
+
+Group Assignment - Shark Tank: Per Scholas Edition 
+
+Oscar Santos, Fouziah Ahmed, and Lakshmi Divya Kolora co-authored this project. 
+
+- Redux implementation in process
+- CSS Template implementation in process
+# LastNetBender_React
---
+++ src/Pages/CharacterGallery.jsx
@@ -0,0 +1,14 @@
+// import { Provider } from "react-redux";
+// import { configureStore } from "@reduxjs/toolkit";
+import Characters from "../Components/Characters";
+// import reducer from "../Store/Posts";
+
+// const store = configureStore(reducer);
+
+export default function CharacterGallery() {
+  return (
+    <div>
+        <Characters />
+    </div>
+  );
+}
